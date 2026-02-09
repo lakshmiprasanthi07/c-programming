7374bef8470e84b1506dadd27acfed9488c737fc
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-02-09 22:28:23 +0530

    Add files via upload

---
+++ 5.print a character.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("%c",ch);
+    scanf("%c",&ch);
+    return 0;
+}
---
+++ 6.print two integers in one line.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int x,y;
+    printf("%d%d",x,y);
+    scanf("%d%d",&x,&y);
+    return 0;
+}
