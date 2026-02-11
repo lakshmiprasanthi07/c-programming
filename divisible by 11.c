200309077e45389d764960253e75206c9d8ebc9e
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-02-11 11:06:28 +0530

    Add files via upload

---
+++ divisible by 11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int num;
+   scanf("%d",&num);
+   if(num%11==0)
+   {
+    printf("divisible by 11");
+   }
+   else
+   {
+    printf("not disible by 11");
+   }
+    return 0;
+}
