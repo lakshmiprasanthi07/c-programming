41c4d2fbd041585e633de17a06b718b096a0d96d
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-02-11 11:55:24 +0530

    Add files via upload

---
+++ number within range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num>=10 && num<=20)
+    {
+        printf("number is within range");
+    }
+else
+printf("number is outside in range");
+    return 0;
+}
