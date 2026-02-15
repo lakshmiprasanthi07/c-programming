c6dcceb44d553e76128996ec454db216ba4b43f0
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-02-15 21:19:58 +0530

    Add files via upload

--- miltiplication table.c
+++ miltiplication table.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int N;
-    long long factorial = 1;
-    scanf("%d", &N);
-
-    for (int i = 1; i <= N; i++) {
-        factorial *= i;
+    int n, i;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
     }
 
-    printf("%lld\n", factorial);
     return 0;
 }
