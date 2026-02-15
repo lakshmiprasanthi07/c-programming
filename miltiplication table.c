b44fd25f5dae68e1129ea96f476868e37e603a3b
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-02-15 21:08:40 +0530

    Add files via upload

---
+++ miltiplication table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    long long factorial = 1;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        factorial *= i;
+    }
+
+    printf("%lld\n", factorial);
+    return 0;
+}
