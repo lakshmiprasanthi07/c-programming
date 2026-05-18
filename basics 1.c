6828080f818f95f81289be1b99d3d3ef94e22f4e
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-05-18 13:56:49 +0530

    Add files via upload

---
+++ basics 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("Hello How are you?");
+    printf("My Name is lakshmi");
+    printf("Today is 19th may 2026");
+    printf("I am at SSCET SALEM");
+    printf("students are learning coding");
+    printf("For sometimes there was no power");
+    printf("Now power is okay");
+        return 0;
+}
---
+++ basics 2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("My Name is Prasanthi\n");
+    printf("Today is 18th may 2026 Monday\n");
+    printf("sangagiri is very hot today\n");
+    printf("some students are late\n");
+    printf("lat hadit is very bad\n");
+    printf("late coming will not be accepted");
+   return 0;
+}
