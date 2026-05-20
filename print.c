ad941aa2c92a8f241526c00919e61361b1923451
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-05-20 11:38:31 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    print("Good morning everyone")
+    printf("This is lakshmi");
+    printf("my full name is BONAMUKKALA LAKSHMI PRASANTHI");
+    printf("my father name is B.Malyadri");
+    printf("he is farmar");
+    printf("my mother name is B.prameela");
+    printf("she is housewife");
+    printf("my favourite colour is navy blue");
+    printf("I have one younger brother");
+    printf("he's name is mahidhar")
+    printf("he is studying 11th standard");
+    printf("my favourate game is kho kho")
+    printf("Today is 19th may 2026");
+    printf("I am at SSCET SALEM");
+    printf("yesterday evening is very cool");
+    printf("sangagiri is very hot today");
+    printf("yesterday have class");
+    printf("today is free");
+    printf("sir will give assignment");
+    printf("all are do sincerly");
+    printf("open idk");
+    printf("select c programming");
+    printf("i doing c programming");
+    printf("complete this assignment fastly");
+    printf("Thank you");
+    return 0;
+}
---
+++ t 3 basics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("My Name is Prasanthi\t");
+    printf("Today is 18th may 2026 Monday\t");
+    printf("sangagiri is very hot today\t");
+    printf("some students are late\t");
+    printf("late habit is very bad\t");
+    printf("late coming will not be accepted\t");
+    printf("six students are badly late");
+   return 0;
+}
