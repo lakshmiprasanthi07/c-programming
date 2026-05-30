42244748b992ee375bec3a260f01e57b07ac49c5
lakshmiprasanthi07 <lakshmiprasanthibonamukkala@gmail.com>
2026-05-30 20:34:44 +0530

    Add files via upload

---
+++ file 2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main() {
+FILE *f1, *f2, *f3;
+int number,i;
+   
+ printf("contents of data file\n\n");
+ f1=fopen("data", "w");
+ for(i=1; i<=30; i++)
+ {
+    scanf("%d",&number);
+    if(number==-1)break;
+    putw(number,f1);
+ }
+ fclose(f1);
+ f1=fopen("data","r");
+ f2=fopen("ODD","w");
+f3=fopen("EVEN","w");
+/*Read from data file*/
+while((number=getw(f1))!=EOF)
+{
+    if(number%2==0)
+    putw(number,f3);    /*write to even file*/
+    else
+    putw(number,f2);   /*write to odd file*/
+}
+fclose(f1);
+fclose(f2);
+fclose(f3);
+f2=fopen("odd", "r");
+f3=fopen("even","r");
+printf("\n\ncontents of odd file\n\n");
+while((number=getw(f2))!=EOF)
+printf("%4d",number);
+printf("\n\n contents of even file\n\n");
+while((number=getw(f3))!=EOF)
+printf("%4d", number);
+fclose(f2);
+fclose(f3);
+}
